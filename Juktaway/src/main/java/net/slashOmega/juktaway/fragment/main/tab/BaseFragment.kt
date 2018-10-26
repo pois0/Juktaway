ca240048943771d87207638d2c27afe1080f0d1d
pois0 <poispois00@gmail.com>
2018-10-26 19:36:43 +0900

    basefragment

--- Juktaway/src/main/java/net/slashOmega/juktaway/fragment/main/tab/BaseFragment.kt
+++ Juktaway/src/main/java/net/slashOmega/juktaway/fragment/main/tab/BaseFragment.kt
@@ -1,4 +1,307 @@
 package net.slashOmega.juktaway.fragment.main.tab
 
-class BaseFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.ListView
+import android.widget.ProgressBar
+import de.greenrobot.event.EventBus
+import kotlinx.android.synthetic.main.pull_to_refresh_list.view.*
+import net.slashOmega.juktaway.R
+import net.slashOmega.juktaway.adapter.TwitterAdapter
+import net.slashOmega.juktaway.event.NewRecordEvent
+import net.slashOmega.juktaway.event.action.GoToTopEvent
+import net.slashOmega.juktaway.event.action.PostAccountChangeEvent
+import net.slashOmega.juktaway.event.action.StatusActionEvent
+import net.slashOmega.juktaway.event.model.StreamingCreateStatusEvent
+import net.slashOmega.juktaway.event.model.StreamingDestroyStatusEvent
+import net.slashOmega.juktaway.event.settings.BasicSettingsChangeEvent
+import net.slashOmega.juktaway.listener.StatusClickListener
+import net.slashOmega.juktaway.listener.StatusLongClickListener
+import net.slashOmega.juktaway.model.AccessTokenManager
+import net.slashOmega.juktaway.model.Row
+import net.slashOmega.juktaway.settings.BasicSettings
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener
+import kotlin.collections.ArrayList
+
+abstract class BaseFragment: Fragment(), OnRefreshListener {
+    protected var mAdapter: TwitterAdapter? = null
+
+    protected var mAutoLoader = false
+    protected var mReloading = false
+    private var mScrolling = false
+    internal var mMaxId = 0L // 読み込んだ最新のツイートID
+    protected var mDirectMessagesMaxId = 0L // 読み込んだ最新の受信メッセージID
+    protected var mSentDirectMessagesMaxId = 0L // 読み込んだ最新の送信メッセージID
+    private val mStackRows = ArrayList<Row>()
+
+    protected lateinit var mListView: ListView
+    protected lateinit var mFooter: ProgressBar
+    protected lateinit var mPullToRefreshLayout: PullToRefreshLayout
+
+    /**
+     * 1. スクロールが終わった瞬間にストリーミングAPIから受信し溜めておいたツイートがあればそれを表示する
+     * 2. スクロールが終わった瞬間に表示位置がトップだったらボタンのハイライトを消すためにイベント発行
+     * 3. スクロール中はスクロール中のフラグを立てる
+     */
+    private val mOnScrollListener = object : AbsListView.OnScrollListener {
+
+        override fun onScrollStateChanged(view: AbsListView, scrollState: Int) {
+            when (scrollState) {
+                AbsListView.OnScrollListener.SCROLL_STATE_IDLE -> {
+                    mScrolling = false
+                    if (mStackRows.size > 0) {
+                        showStack()
+                    } else if (isTop) {
+                        EventBus.getDefault().post(GoToTopEvent())
+                    }
+                }
+                AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL, AbsListView.OnScrollListener.SCROLL_STATE_FLING -> mScrolling = true
+            }
+        }
+
+        override fun onScroll(view: AbsListView, firstVisibleItem: Int, visibleItemCount: Int, totalItemCount: Int) {
+            // 最後までスクロールされたかどうかの判定
+            if (totalItemCount > 0 && totalItemCount == firstVisibleItem + visibleItemCount) {
+                additionalReading()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+        = inflater.inflate(R.layout.pull_to_refresh_list, container, false)?.apply { activity?.let { act ->
+        mListView = list_view.apply {
+            onItemClickListener = StatusClickListener(act)
+            onItemLongClickListener = StatusLongClickListener(act)
+            setOnScrollListener(mOnScrollListener)
+        }
+        mFooter = guruguru.apply { visibility = View.GONE }
+        mPullToRefreshLayout = ptr_layout
+
+        ActionBarPullToRefresh.from(act)
+                .theseChildrenArePullable(mListView)
+                .listener(this@BaseFragment)
+                .setup(mPullToRefreshLayout)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        /**
+         * mMainPagerAdapter.notifyDataSetChanged() された時に
+         * onCreateView と onActivityCreated がインスタンスが生きたまま呼ばれる
+         * 多重に初期化処理を実行しないように変数チェックを行う
+         */
+        if (mAdapter == null) {
+            // Status(ツイート)をViewに描写するアダプター
+            mAdapter = TwitterAdapter(activity, R.layout.row_tweet)
+            mListView.visibility = View.GONE
+            taskExecute()
+        }
+
+        mListView.adapter = mAdapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        EventBus.getDefault().register(this)
+    }
+
+    override fun onPause() {
+        EventBus.getDefault().unregister(this)
+        super.onPause()
+    }
+
+    override fun onRefreshStarted(view: View) {
+        reload()
+    }
+
+    fun reload() {
+        mReloading = true
+        mPullToRefreshLayout.isRefreshing = true
+        taskExecute()
+    }
+
+    fun clear() {
+        mMaxId = 0L
+        mDirectMessagesMaxId = 0L
+        mSentDirectMessagesMaxId = 0L
+        mAdapter?.clear()
+    }
+
+    protected fun additionalReading() {
+        if (!mAutoLoader || mReloading) return
+        mFooter.visibility = View.VISIBLE
+        mAutoLoader = false
+        taskExecute()
+    }
+
+    fun goToTop(): Boolean {
+        mListView.setSelection(0)
+        return if (mStackRows.size > 0) {
+            showStack()
+            false
+        } else true
+    }
+
+    var isTop = mListView.firstVisiblePosition == 0
+
+    /**
+     * ツイートの表示処理、画面のスクロール位置によって適切な処理を行う、まだバグがある
+     */
+    private val mRender = Runnable {
+        if (mScrolling || mAdapter == null) return@Runnable
+
+        // 表示している要素の位置
+        val position = mListView.firstVisiblePosition
+
+        // 縦スクロール位置
+        val view = mListView.getChildAt(0)
+        val y = view?.top ?: 0
+
+        // 要素を上に追加（ addだと下に追加されてしまう ）
+        var highlight = false
+        mStackRows.forEach { row ->
+            mAdapter?.insert(row, 0)
+            when {
+                row.isFavorite -> {
+                    // お気に入りしたのが自分じゃない時
+                    if (row.source!!.id != AccessTokenManager.getUserId()) highlight = true
+                }
+                row.isStatus -> {
+                    // 投稿主が自分じゃない時
+                    if (row.status!!.user.id != AccessTokenManager.getUserId()) highlight = true
+                }
+                row.isDirectMessage -> {
+                    // 投稿主が自分じゃない時
+                    if (row.message!!.senderId != AccessTokenManager.getUserId()) highlight = true
+                }
+            }
+        }
+        mStackRows.clear()
+
+        val autoScroll = position == 0 && y == 0 && mStackRows.size < 3
+
+        if (highlight) EventBus.getDefault().post(NewRecordEvent(getTabId(), getSearchWord(), autoScroll))
+
+        if (autoScroll) {
+            mListView.setSelection(0)
+        } else {
+            // 少しでもスクロールさせている時は画面を動かさない様にスクロー位置を復元する
+            mListView.setSelectionFromTop(position + mStackRows.size, y)
+            // 未読の新規ツイートをチラ見せ
+            if (position == 0 && y == 0) {
+                mListView.smoothScrollToPositionFromTop(position + mStackRows.size, 120)
+            }
+        }
+    }
+
+    /**
+     * 新しいツイートを表示して欲しいというリクエストを一旦待たせ、
+     * 250ms以内に同じリクエストが来なかったら表示する。
+     * 250ms以内に同じリクエストが来た場合は、更に250ms待つ。
+     * 表示を連続で行うと処理が重くなる為この制御を入れている。
+     */
+    private fun showStack() {
+        mListView.removeCallbacks(mRender)
+        mListView.postDelayed(mRender, 250)
+    }
+
+    /**
+     * ストリーミングAPIからツイートやメッセージを受信した時の処理
+     * 1. 表示スべき内容かチェックし、不適切な場合はスルーする
+     * 2. すぐ表示すると流速が早い時にガクガクするので溜めておく
+     *
+     * @param row ツイート情報
+     */
+    fun addStack(row: Row) {
+        if (isSkip(row)) return
+        mStackRows.add(row)
+        if (!mScrolling && isTop) {
+            showStack()
+        } else {
+            EventBus.getDefault().post(NewRecordEvent(getTabId(), getSearchWord(), false))
+        }
+    }
+
+    /**
+     * そのツイート（またはメッセージ）を表示するかどうかのチェック
+     */
+    protected abstract fun isSkip(row: Row): Boolean
+
+    /**
+     * タブ固有のID、ユーザーリストではリストのIDを、その他はマイナスの固定値を返す
+     */
+    abstract fun getTabId(): Long
+
+    fun getSearchWord(): String = ""
+
+
+    /**
+     * 読み込み用のAsyncTaskを実行する
+     */
+    protected abstract fun taskExecute()
+
+    /**
+     *
+     * !!! EventBus !!!
+     *
+     */
+
+
+    /**
+     * 高速スクロールの設定が変わったら切り替える
+     */
+    fun onEventMainThread(event: BasicSettingsChangeEvent) { mListView.isFastScrollEnabled = BasicSettings.fastScrollOn }
+
+    fun onEventMainThread(event: StatusActionEvent) { mAdapter?.notifyDataSetChanged() }
+
+    /**
+     * ストリーミングAPIからツイ消しイベントを受信
+     *
+     * @param event ツイート
+     */
+    fun onEventMainThread(event: StreamingDestroyStatusEvent) {
+        val removePositions = mAdapter?.removeStatus(event.statusId!!) ?: ArrayList(0)
+        for (removePosition in removePositions) {
+            if (removePosition >= 0) {
+                val visiblePosition = mListView.firstVisiblePosition
+                if (visiblePosition > removePosition) {
+                    val view = mListView.getChildAt(0)
+                    val y = view?.top ?: 0
+                    mListView.setSelectionFromTop(visiblePosition - 1, y)
+                    break
+                }
+            }
+        }
+    }
+
+    /**
+     * ストリーミングAPIからツイートイベントを受信
+     *
+     * @param event ツイート
+     */
+    fun onEventMainThread(event: StreamingCreateStatusEvent) {
+        addStack(event.row)
+    }
+
+    /**
+     * アカウント変更通知を受け、表示中のタブはリロード、表示されていたいタブはクリアを行う
+     *
+     * @param event アプリが表示しているタブのID
+     */
+    fun onEventMainThread(event: PostAccountChangeEvent) {
+        if (event.tabId == getTabId()) reload() else clear()
+    }
 }
