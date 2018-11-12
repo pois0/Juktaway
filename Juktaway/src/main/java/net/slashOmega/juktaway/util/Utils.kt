060b43c19db120c571d5d8abc5f5598f2e50e8e5
pois0 <poispois00@gmail.com>
2018-11-13 02:41:50 +0900

    Kotlize RegisterListAdapter

---
+++ Juktaway/src/main/java/net/slashOmega/juktaway/util/Utils.kt
@@ -0,0 +1,15 @@
+package net.slashOmega.juktaway.util
+
+/**
+ * Created on 2018/11/13.
+ */
+
+inline fun <T, R> T.tryAndTrace (block: T.() -> R): Boolean = runCatching { block() }.run {
+    exceptionOrNull()?.printStackTrace()
+    isSuccess
+}
+
+inline fun <R> tryAndTrace (block: () -> R): Boolean = runCatching { block() }.run {
+    exceptionOrNull()?.printStackTrace()
+    isSuccess
+}
