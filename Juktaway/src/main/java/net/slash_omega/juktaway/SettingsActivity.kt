955031ae8010a785cb03991e112070057664dc56
pois0 <poispois00@gmail.com>
2019-06-10 05:05:31 +0900

    Refactor: remove unused activity classes

--- Juktaway/src/main/java/net/slash_omega/juktaway/SettingsActivity.kt
+++ Juktaway/src/main/java/net/slash_omega/juktaway/SettingsActivity.kt
@@ -1,12 +1,7 @@
 package net.slash_omega.juktaway
 
 import android.app.Activity
-import android.app.AlertDialog
-import android.app.Dialog
-import android.app.DialogFragment
-import android.content.Context
 import android.os.Bundle
-import android.preference.ListPreference
 import android.preference.PreferenceFragment
 import android.view.MenuItem
 import net.slash_omega.juktaway.util.ThemeUtil
@@ -16,85 +11,13 @@ import net.slash_omega.juktaway.util.ThemeUtil
  */
 class SettingsActivity: Activity() {
     class SettingsFragment: PreferenceFragment() {
-        companion object {
-            fun changedPreference(longTapPreference: ListPreference) {
-                longTapPreference.setOnPreferenceChangeListener { pref, newValue ->
-                    (pref as ListPreference).apply {
-                        entries?.let {
-                            summary = it[findIndexOfValue(newValue as String)]
-                        }
-                    }
-                    true
-                }
-            }
-        }
-
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
 
             if (preferenceManager == null) return
             preferenceManager.sharedPreferencesName = "settings"
 
-            addPreferencesFromResource(R.xml.pref_newgeneral)
-
-            (findPreference("display_account_name") as? ListPreference)?.apply {
-                summary = entry
-                setOnPreferenceChangeListener { pref, newValue ->
-                    (pref as ListPreference).apply {
-                        entries?.let {
-                            summary = it[findIndexOfValue(newValue as String)]
-                        }
-                    }
-                    true
-                }
-            } ?: return
-
-            (findPreference("font_size") as? ListPreference)?.apply {
-                summary = "$value pt"
-                setOnPreferenceChangeListener { pref, newValue ->
-                    pref.summary = "$newValue pt"
-                    true
-                }
-            } ?: return
-            (findPreference("long_tap") as? ListPreference)?.apply {
-                summary = entry
-                changedPreference(this)
-            } ?: return
-            (findPreference("themeName") as? ListPreference)?.apply {
-                summary = entry
-                setOnPreferenceChangeListener { pref, newValue ->
-                    (pref as ListPreference).apply {
-                        entries?.let {
-                            summary = it[findIndexOfValue(newValue as String)]
-                        }
-                        fragmentManager?.let {
-                            SettingsActivity.ThemeSwitchDialogFragment().show(it, "dialog")
-                        }
-                    }
-                    true
-                }
-            }
-        }
-    }
-
-    class ThemeSwitchDialogFragment: DialogFragment() {
-        private var mActivity: SettingsActivity? = null
-
-        override fun onAttach(context: Context) {
-            super.onAttach(context)
-
-            mActivity = context as SettingsActivity
-        }
-
-        override fun onCreateDialog(savedInstanceState:Bundle): Dialog {
-            return AlertDialog.Builder(mActivity)
-                    .setMessage(R.string.confirm_theme_apply_finish)
-                    .setPositiveButton(getString(R.string.button_yes)) { _, _ ->
-                        mActivity?.applyTheme()
-                        dismiss()
-                    }
-                    .setNegativeButton(getString(R.string.button_no)) { _, _ -> dismiss() }
-                    .create()
+            addPreferencesFromResource(R.xml.pref_general)
         }
     }
 
@@ -115,10 +38,5 @@ class SettingsActivity: Activity() {
             android.R.id.home -> finish()
         }
         return true
-    }
-
-    fun applyTheme() {
-        setResult(Activity.RESULT_OK)
-        finish()
     }
 }
